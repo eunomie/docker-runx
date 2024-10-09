08ebcb0b4691d1812c24b0f9fa6f88de0dd454af
Yves Brissaud <yves.brissaud@gmail.com>
2024-10-09 22:31:58 +0200

    feat: allow to decorate scratch
    Just creates an index with only runx related image
    
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- runkit/decorate.go
+++ runkit/decorate.go
@@ -30,57 +30,69 @@ func Decorate(ctx context.Context, src, dest string, runxConfig, runxDoc []byte)
 		return fmt.Errorf("could not create runx image: %w", err)
 	}
 
-	desc, err = remote.Get(ref, remoteOpts...)
-	if err != nil {
-		return fmt.Errorf("could not get image %s: %w", src, err)
-	}
-
-	if desc.MediaType.IsImage() {
-		img, err := remote.Image(ref, remoteOpts...)
-		if err != nil {
-			return fmt.Errorf("could not get image %s: %w", src, err)
-		}
-		configFile, _ := img.ConfigFile()
-		imgDesc := desc.Descriptor
-		imgDesc.Platform = configFile.Platform()
-
+	if src == "scratch" {
 		index = // create a manifest
 		mutate.AppendManifests(
 			// as an index
 			mutate.IndexMediaType(empty.Index, types.OCIImageIndex),
-			// with the referenced image
-			mutate.IndexAddendum{
-				Add:        img,
-				Descriptor: imgDesc,
-			},
 			// and the new runx image
 			mutate.IndexAddendum{
 				Add:        runxImage,
 				Descriptor: *runxDesc,
 			})
-	} else if desc.MediaType.IsIndex() {
-		index, err = remote.Index(ref, remoteOpts...)
-		if err != nil {
-			return fmt.Errorf("could not get image index %s: %w", src, err)
-		}
-
-		// remove existing runx manifest
-		manifests, _ := index.IndexManifest()
-		for _, manifest := range manifests.Manifests {
-			if _, ok := manifest.Annotations[runkit.RunxManifestType]; ok {
-				index = mutate.RemoveManifests(index, match.Digests(manifest.Digest))
-			}
-		}
-
-		// add the new runx manifest
-		index = mutate.AppendManifests(
-			index,
-			mutate.IndexAddendum{
-				Add:        runxImage,
-				Descriptor: *runxDesc,
-			})
 	} else {
-		return fmt.Errorf("unsupported media type %s", desc.MediaType)
+		desc, err = remote.Get(ref, remoteOpts...)
+		if err != nil {
+			return fmt.Errorf("could not get image %s: %w", src, err)
+		}
+
+		if desc.MediaType.IsImage() {
+			img, err := remote.Image(ref, remoteOpts...)
+			if err != nil {
+				return fmt.Errorf("could not get image %s: %w", src, err)
+			}
+			configFile, _ := img.ConfigFile()
+			imgDesc := desc.Descriptor
+			imgDesc.Platform = configFile.Platform()
+
+			index = // create a manifest
+			mutate.AppendManifests(
+				// as an index
+				mutate.IndexMediaType(empty.Index, types.OCIImageIndex),
+				// with the referenced image
+				mutate.IndexAddendum{
+					Add:        img,
+					Descriptor: imgDesc,
+				},
+				// and the new runx image
+				mutate.IndexAddendum{
+					Add:        runxImage,
+					Descriptor: *runxDesc,
+				})
+		} else if desc.MediaType.IsIndex() {
+			index, err = remote.Index(ref, remoteOpts...)
+			if err != nil {
+				return fmt.Errorf("could not get image index %s: %w", src, err)
+			}
+
+			// remove existing runx manifest
+			manifests, _ := index.IndexManifest()
+			for _, manifest := range manifests.Manifests {
+				if _, ok := manifest.Annotations[runkit.RunxManifestType]; ok {
+					index = mutate.RemoveManifests(index, match.Digests(manifest.Digest))
+				}
+			}
+
+			// add the new runx manifest
+			index = mutate.AppendManifests(
+				index,
+				mutate.IndexAddendum{
+					Add:        runxImage,
+					Descriptor: *runxDesc,
+				})
+		} else {
+			return fmt.Errorf("unsupported media type %s", desc.MediaType)
+		}
 	}
 
 	err = remote.WriteIndex(destRef, index, remoteOpts...)
