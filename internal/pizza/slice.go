b2038780861d3765b187cdbcf0c3369ad73b2c4b
Yves Brissaud <yves.brissaud@gmail.com>
2024-10-17 18:27:50 +0200

    feat: correctly handle yaml strings
    Both > and | are handled
    I suggest to only use | with \ at the end of the line, so the command
    will be easier to read and copy paste for the user.
    But > will be handled and newlines will be removed.
    
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- internal/pizza/slice.go
+++ internal/pizza/slice.go
@@ -1,6 +1,9 @@
 package pizza
 
 func Map[I, R interface{}](objs []I, fn func(obj I) R) []R {
+	if len(objs) == 0 {
+		return nil
+	}
 	s := []R{}
 	for _, obj := range objs {
 		s = append(s, fn(obj))
